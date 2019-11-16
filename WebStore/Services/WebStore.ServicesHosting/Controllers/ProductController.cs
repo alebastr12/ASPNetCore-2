54e58de6b313073c39f84edb0f56d8cf0ef448ec
alebastr12 <thegrace12@gmail.com>
2019-11-16 16:33:44 +0300

    документация к web api средствами swagger

--- WebStore/Services/WebStore.ServicesHosting/Controllers/ProductController.cs
+++ WebStore/Services/WebStore.ServicesHosting/Controllers/ProductController.cs
@@ -16,23 +16,55 @@ namespace WebStore.ServicesHosting.Controllers
     public class ProductController : ControllerBase, IProductService
     {
         private readonly IProductService productService;
-
+        /// <summary>
+        /// Конструктор сервиса
+        /// </summary>
+        /// <param name="productService">Сервис работы с продуктами</param>
         public ProductController(IProductService productService)
         {
             this.productService = productService;
         }
+        /// <summary>
+        /// Добавить продукт в базу
+        /// </summary>
+        /// <param name="item">Продукт для добавления</param>
         [HttpPost("add")]
         public void AddProduct([FromBody] ProductDTO item) => productService.AddProduct(item);
+        /// <summary>
+        /// 
+        /// </summary>
         [NonAction]
         public void Commit() { }
+        /// <summary>
+        /// Удалить продукт из базы
+        /// </summary>
+        /// <param name="id">Идентификатор продукта, который нужно удалить</param>
         [HttpDelete("{id?}")]
         public void Delete(int? id) => productService.Delete(id);
+        /// <summary>
+        /// Получить список брэндов
+        /// </summary>
+        /// <returns>Список всех брэндов</returns>
         [HttpGet("brands")]
         public IEnumerable<BrandDTO> GetBrands() => productService.GetBrands();
+        /// <summary>
+        /// Получить список категорий
+        /// </summary>
+        /// <returns>Список всех категорий</returns>
         [HttpGet("categoryes")]
         public IEnumerable<CategoryDTO> GetCategories() => productService.GetCategories();
+        /// <summary>
+        /// Получить продукт по его идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор продукта</param>
+        /// <returns>Продукт по заданному идентификатору</returns>
         [HttpGet("{id}")]
         public ProductDTO GetProductById(int id) => productService.GetProductById(id);
+        /// <summary>
+        /// Получить список продуктов по заданному фильтру
+        /// </summary>
+        /// <param name="filter">Фильтр запроса</param>
+        /// <returns>Список продуктов</returns>
         [HttpPost]
         public IEnumerable<ProductDTO> GetProducts([FromBody] ProductFilter filter) => productService.GetProducts(filter);
     }
--- WebStore/Services/WebStore.ServicesHosting/Startup.cs
+++ WebStore/Services/WebStore.ServicesHosting/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using WebStore.DAL;
 using WebStore.Domain.Entitys;
 using WebStore.Interfaces.Services;
@@ -33,7 +34,15 @@ namespace WebStore.ServicesHosting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+
+            services.AddSwaggerGen(
+                opt =>
+                {
+                    opt.SwaggerDoc("v1", new Info { Title = "WebStore.API", Version = "v1" });
+                    opt.IncludeXmlComments("WebStore.ServicesHosting.xml");
+                    opt.IncludeXmlComments(@"bin\Debug\netcoreapp2.2\WebStore.Domain.xml");
+                });
+
             services.AddDbContext<WebStoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<User, IdentityRole>()
@@ -70,7 +79,13 @@ namespace WebStore.ServicesHosting
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                opt =>
+                {
+                    opt.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.API");
+                    opt.RoutePrefix = string.Empty;
+                });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
